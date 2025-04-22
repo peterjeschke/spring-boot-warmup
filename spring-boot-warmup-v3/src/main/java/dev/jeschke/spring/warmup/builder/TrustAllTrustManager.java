4bcb7510f6bc8d85b56b3c4f8ee0992224c9b203
Peter-René Jeschke <peter-rene.jeschke@ratepay.com>
2025-04-22 09:44:50 +0200

    Suppress sonar warning about disabling TLS
    Signed-off-by: Peter-René Jeschke <peter-rene.jeschke@ratepay.com>

--- spring-boot-warmup-v3/src/main/java/dev/jeschke/spring/warmup/builder/TrustAllTrustManager.java
+++ spring-boot-warmup-v3/src/main/java/dev/jeschke/spring/warmup/builder/TrustAllTrustManager.java
@@ -3,12 +3,17 @@ package dev.jeschke.spring.warmup.builder;
 import java.security.cert.X509Certificate;
 import javax.net.ssl.X509TrustManager;
 
+@SuppressWarnings("java:S4830")
 public class TrustAllTrustManager implements X509TrustManager {
     @Override
-    public void checkClientTrusted(final X509Certificate[] chain, final String authType) {}
+    public void checkClientTrusted(final X509Certificate[] chain, final String authType) {
+        // Don't check certificates
+    }
 
     @Override
-    public void checkServerTrusted(final X509Certificate[] chain, final String authType) {}
+    public void checkServerTrusted(final X509Certificate[] chain, final String authType) {
+        // Don't check certificates
+    }
 
     @Override
     public X509Certificate[] getAcceptedIssuers() {
